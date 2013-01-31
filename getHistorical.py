f6f23af53b7de9dc513d3b54bd74d1d7d6fadb55
Raymond <raytochina@gmail.com>
2013-01-30 18:09:13 -0800

    Working Crawler
    Downloads a file, then ready to connect to DB

--- getHistorical.py
+++ getHistorical.py
@@ -1,7 +1,8 @@
 
 # stock crawler, get from list of stocks, download historical data
 import urllib2
-
+import os
+import csv
 
 stocks = ["AAPL"]
 
@@ -10,13 +11,39 @@ stocks = ["AAPL"]
 #for i in range(len(stocks)):
 
 
-def 
-    url = "http://ichart.finance.yahoo.com/table.csv?s="+stocks[i]+"&d=0&e=30&f=2013&g=d&a=8&b=7&c=1984&ignore=.csv"
-    response = urllib2.urlopen(url)
-    
-    localFile = open(stocks[i], 'w')
-    localFile.write(response.read())
-    localFile.close()
+#def getHistorical(stock):
+for i in range(1):
+	stock = "AAPL"
+
+	dir = "./stockHistorical/"
+	#Dependent on Yahoo finance to get historical data
+	url = "http://ichart.finance.yahoo.com/table.csv?s="+stock+"&d=0&e=30&f=2013&g=d&a=8&b=7&c=1984&ignore=.csv"
+	response = urllib2.urlopen(url)
+	localFile = open(dir+stock, 'w')
+	localFile.write(response.read())
+	localFile.close()
+	filereader = csv.reader(open(dir+stock,'rb'), delimiter=",")
+	
+	#setup variables
+	#Name of Stock
+	#Date,Open,High,Low,Close,Volume,Adj Close
+	for document in filereader:
+		name 		= stock
+		date 		= document[0]
+		openPrice 	= document[1]
+		highPrice	= document[2]
+		lowPrice	= document[3]
+		closingPrice= document[4]
+		volume 		= document[5]
+		adjClose	= document[6]
+
+		print "Name: "+stock+ " date "+ date + " closingPrice " + closingPrice
+
+
+
+
+
+#print getHistorical("AAPL")
 
 
 
